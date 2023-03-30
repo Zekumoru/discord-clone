2e04e6de247d18a19fcb7ed0ba50e6582c8c2883
Zekumoru Dragonhart <dev@zekumoru.com>
2023-03-30 21:17:52 +0200

    Add "gg sans" font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        unisans: ['Uni Sans', 'sans-serif'],
+        sans: ['gg sans', ...defaultTheme.fontFamily.sans],
+        unisans: ['Uni Sans', ...defaultTheme.fontFamily.mono],
       },
       colors: {
         background: {
