bda595f5692f517d6c0cd8d05afd0053b73eaa6e
Zekumoru Dragonhart <dev@zekumoru.com>
2023-03-30 13:01:18 +0200

    Initial commit

---
+++ src/firebase.config.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBwAeFkfSCN9PckGhUWAtgoiAZ_x0axT_o',
+  authDomain: 'discord-clone-zekumoru.firebaseapp.com',
+  projectId: 'discord-clone-zekumoru',
+  storageBucket: 'discord-clone-zekumoru.appspot.com',
+  messagingSenderId: '737096461446',
+  appId: '1:737096461446:web:d3a598ac5004cc5ea73507',
+};
+
+export default firebaseConfig;
