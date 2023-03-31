3ffcbb70925a127f498bceed5ec6044a8d6e7a4e
Zekumoru Dragonhart <dev@zekumoru.com>
2023-03-31 22:15:19 +0200

    Create ScreenModal context

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { Route, Routes } from 'react-router-dom';
 import Authentication from './pages/authentication/Authentication';
 import Friends from './pages/channels/pages/friends/Friends';
 import Channels from './pages/channels/Channels';
+import ScreenModalProvider from './contexts/ScreenModal';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/*" element={<Authentication />} />
-        <Route path="/channels/*" element={<Channels />} />
-      </Routes>
+      <ScreenModalProvider>
+        <Routes>
+          <Route path="/*" element={<Authentication />} />
+          <Route path="/channels/*" element={<Channels />} />
+        </Routes>
+      </ScreenModalProvider>
     </div>
   );
 }
