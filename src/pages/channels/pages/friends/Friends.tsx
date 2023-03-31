4fd2d1974c7829288920dd06909c543d95f321f7
Zekumoru Dragonhart <dev@zekumoru.com>
2023-03-31 19:47:44 +0200

    Create friends page

--- src/pages/channels/pages/friends/Friends.tsx
+++ src/pages/channels/pages/friends/Friends.tsx
@@ -1,11 +1,39 @@
 import IconUserPlus from '../../../../assets/icons/IconUserPlus';
+import snowflakeId from '../../../../utils/snowflake-id/snowflakeId';
 import Toolbar from '../../components/Toolbar';
+import FriendItem from './components/FriendItem';
+import IFriend from './types';
+
+const createFriend = (name: string) => {
+  return {
+    name,
+    userId: snowflakeId(),
+    chatId: snowflakeId(),
+    photoUrl: null,
+  } as IFriend;
+};
+
+const friends = [createFriend('Foo'), createFriend('Bar'), createFriend('Baz')];
 
 const Friends = () => {
   return (
-    <div>
+    <>
       <Toolbar buttons={<IconUserPlus className="h-6 w-6" />}>Friends</Toolbar>
-    </div>
+
+      <div className="py-4">
+        <h2 className="heading-2 px-4">Added — 3</h2>
+        <ul>
+          {friends.map((friend) => (
+            <FriendItem key={friend.userId} friend={friend} />
+          ))}
+        </ul>
+
+        <h2 className="heading-2 mt-4 px-4">Requests — 1</h2>
+        <ul>
+          <FriendItem friend={createFriend('Foobar')} />
+        </ul>
+      </div>
+    </>
   );
 };
 
---
+++ src/pages/channels/pages/friends/components/FriendItem.tsx
@@ -0,0 +1,20 @@
+import IconChatBubble from '../../../../../assets/icons/IconChatBubble';
+import IFriend from '../types';
+
+type FriendItemProps = {
+  friend: IFriend;
+};
+
+const FriendItem = ({ friend }: FriendItemProps) => {
+  return (
+    <li className="li-rule-85 relative flex items-center gap-2 px-4 py-2">
+      <span className="inline-block h-10 w-10 rounded-full bg-neutral-600" />
+      <span className="mr-auto font-semibold">{friend.name}</span>
+      <span className="grid h-8 w-8 place-content-center rounded-full bg-background-500">
+        <IconChatBubble className="h-4 w-4" />
+      </span>
+    </li>
+  );
+};
+
+export default FriendItem;
