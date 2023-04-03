ba7d6873eeb0d38217d43c0f83f9e54ab4d64fca
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-03 17:40:23 +0200

    Extract logic of slicing the tag from username

--- src/pages/channels/pages/friends/components/friend-item/FriendItem.tsx
+++ src/pages/channels/pages/friends/components/friend-item/FriendItem.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import useUser from '../../../../../../types/user/hooks/useUser';
+import removeTagFromName from '../../../../../../utils/removeTagFromName';
 
 type FriendItemProps = {
   friendId: string | undefined;
@@ -13,7 +14,7 @@ const FriendItem = ({ friendId, buttons }: FriendItemProps) => {
     <li className="li-rule-85 relative flex items-center gap-2 px-4 py-2">
       <span className="inline-block h-10 w-10 rounded-full bg-neutral-600" />
       <span className="mr-auto font-semibold">
-        {friend?.username.slice(0, -5)}
+        {removeTagFromName(friend?.username ?? '')}
       </span>
       {buttons}
     </li>
---
+++ src/utils/removeTagFromName.ts
@@ -0,0 +1,5 @@
+const removeTagFromName = (name: string) => {
+  return name.slice(0, -5);
+};
+
+export default removeTagFromName;
