72da5d57249be6d62df56edbd3cf978ae42b371e
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-03 11:54:03 +0200

    Refactor tests of current user contest

--- src/contexts/current-user/__test__/CurrentUserContext.test.tsx
+++ src/contexts/current-user/__test__/CurrentUserContext.test.tsx
@@ -2,44 +2,41 @@ import { render, screen, waitFor } from '@testing-library/react';
 import CurrentUserProvider, { useCurrentUser } from '../CurrentUserContext';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { User } from 'firebase/auth';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QuerySnapshot, getDocs } from 'firebase/firestore';
+import NoRetryQueryClientProvider from '../../../tests/NoRetryQueryClientProvider';
 import IUser from '../../../types/user/User';
-import findUserByFirebaseId from '../../../types/user/firebase/findUserByFirebaseId';
-
-vi.mock('../../../types/user/firebase/findUserByFirebaseId', () => ({
-  default: vi.fn(() => undefined),
-}));
 
 vi.mock('firebase/auth');
+vi.mock('firebase/firestore');
 vi.mock('react-firebase-hooks/auth');
-
-const mockedUseAuthState = vi.mocked(useAuthState);
-const mockedFindUserByFirebaseId = vi.mocked(findUserByFirebaseId);
-
-afterEach(() => {
-  mockedUseAuthState.mockReset();
-  mockedFindUserByFirebaseId.mockReset();
-});
 
 describe('CurrentUserContext', () => {
   it('should return the current user', async () => {
-    mockedUseAuthState.mockReturnValue([
+    vi.mocked(useAuthState).mockReturnValue([
       { uid: '1234567890' } as User,
       false,
       undefined,
     ]);
-    mockedFindUserByFirebaseId.mockResolvedValue({ username: 'user' } as IUser);
-    const queryClient = new QueryClient();
+    vi.mocked(getDocs<IUser>).mockResolvedValue({
+      docs: [
+        {
+          data: () => ({
+            id: '1234567890',
+            username: 'User',
+          }),
+        },
+      ],
+    } as QuerySnapshot<IUser>);
     const Component = () => {
       const [user] = useCurrentUser();
       return <div>{user?.username}</div>;
     };
     render(
-      <QueryClientProvider client={queryClient}>
+      <NoRetryQueryClientProvider>
         <CurrentUserProvider>
           <Component />
         </CurrentUserProvider>
-      </QueryClientProvider>
+      </NoRetryQueryClientProvider>
     );
 
     await waitFor(() => {
@@ -48,18 +45,17 @@ describe('CurrentUserContext', () => {
   });
 
   it('should show loading if firebase is loading', async () => {
-    mockedUseAuthState.mockReturnValue([undefined, true, undefined]);
-    const queryClient = new QueryClient();
+    vi.mocked(useAuthState).mockReturnValue([undefined, true, undefined]);
     const Component = () => {
       const [_user, loading] = useCurrentUser();
       return <div>{loading && 'Loading...'}</div>;
     };
     render(
-      <QueryClientProvider client={queryClient}>
+      <NoRetryQueryClientProvider>
         <CurrentUserProvider>
           <Component />
         </CurrentUserProvider>
-      </QueryClientProvider>
+      </NoRetryQueryClientProvider>
     );
 
     await waitFor(() => {
@@ -68,22 +64,21 @@ describe('CurrentUserContext', () => {
   });
 
   it('should show loading if the query is loading', async () => {
-    mockedUseAuthState.mockReturnValue([
+    vi.mocked(useAuthState).mockReturnValue([
       { uid: '1234567890' } as User,
       false,
       undefined,
     ]);
-    const queryClient = new QueryClient();
     const Component = () => {
       const [_user, loading] = useCurrentUser();
       return <div>{loading && 'Loading...'}</div>;
     };
     render(
-      <QueryClientProvider client={queryClient}>
+      <NoRetryQueryClientProvider>
         <CurrentUserProvider>
           <Component />
         </CurrentUserProvider>
-      </QueryClientProvider>
+      </NoRetryQueryClientProvider>
     );
 
     await waitFor(() => {
@@ -92,21 +87,24 @@ describe('CurrentUserContext', () => {
   });
 
   it('it should return error from firebase', async () => {
-    mockedUseAuthState.mockReturnValue([undefined, false, new Error('error')]);
-    const queryClient = new QueryClient();
+    vi.mocked(useAuthState).mockReturnValue([
+      undefined,
+      false,
+      new Error('error'),
+    ]);
     const Component = () => {
       const [_user, _loading, error] = useCurrentUser();
       if (error instanceof Error) {
         return <div>{error.message}</div>;
       }
-      return <div></div>;
+      return <div />;
     };
     render(
-      <QueryClientProvider client={queryClient}>
+      <NoRetryQueryClientProvider>
         <CurrentUserProvider>
           <Component />
         </CurrentUserProvider>
-      </QueryClientProvider>
+      </NoRetryQueryClientProvider>
     );
 
     await waitFor(() => {
@@ -115,36 +113,29 @@ describe('CurrentUserContext', () => {
   });
 
   it('it should return error from the query', async () => {
-    mockedUseAuthState.mockReturnValue([
+    vi.mocked(useAuthState).mockReturnValue([
       { uid: '1234567890' } as User,
       false,
       undefined,
     ]);
-    mockedFindUserByFirebaseId.mockImplementation(() => {
+    vi.mocked(getDocs).mockImplementation((_q) => {
       throw new Error('error');
-    });
-    const queryClient = new QueryClient({
-      defaultOptions: {
-        queries: {
-          retry: false,
-        },
-      },
     });
     const Component = () => {
       const [_user, _loading, error] = useCurrentUser();
       if (error instanceof Error) {
         return <div>{error.message}</div>;
       }
-      return <div></div>;
+      return <div />;
     };
     const originalConsoleError = console.error;
     console.error = () => {};
     render(
-      <QueryClientProvider client={queryClient}>
+      <NoRetryQueryClientProvider>
         <CurrentUserProvider>
           <Component />
         </CurrentUserProvider>
-      </QueryClientProvider>
+      </NoRetryQueryClientProvider>
     );
 
     await waitFor(() => {
