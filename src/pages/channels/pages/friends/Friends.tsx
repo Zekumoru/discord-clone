aa75f5f6a69d26f42165b809c0833aa8c5ba59da
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-03 16:56:02 +0200

    Create useFriends hook

--- src/pages/channels/pages/friends/Friends.tsx
+++ src/pages/channels/pages/friends/Friends.tsx
@@ -1,87 +1,28 @@
-import {
-  IconChatBubble,
-  IconCheck,
-  IconUserPlus,
-  IconXMark,
-} from '../../../../assets/icons';
-import { useScreenModal } from '../../../../contexts/screen-modal/ScreenModalContext';
-import IFriend from '../../../../types/friend/Friend';
-import snowflakeId from '../../../../utils/snowflake-id/snowflakeId';
-import AddFriendScreenModal from './components/add-friend-screen-modal/AddFriendScreenModal';
-import Toolbar from '../../components/Toolbar';
-import CircledIconButton from './components/CircledIconButton';
-import FriendItem from './components/FriendItem';
-
-const createFriend = () => {
-  return {
-    userId: snowflakeId(),
-    chatId: snowflakeId(),
-  } as IFriend;
-};
-
-const friends = [createFriend(), createFriend(), createFriend()];
+import { useCurrentUser } from '../../../../contexts/current-user/CurrentUserContext';
+import FriendsToolbar from './components/FriendsToolbar';
+import UserFriendItem from './components/friend-item/UserFriendItem';
+import useFriends from './hooks/useFriends';
 
 const Friends = () => {
-  const [openModal, closeModal] = useScreenModal();
+  const [user] = useCurrentUser();
+  const [friends] = useFriends(user?.friendsId);
 
   return (
     <>
-      <Toolbar
-        buttons={
-          <div
-            onClick={() =>
-              openModal(<AddFriendScreenModal close={closeModal} />)
-            }
-          >
-            <IconUserPlus className="h-6 w-6" />
-          </div>
-        }
-      >
-        Friends
-      </Toolbar>
+      <FriendsToolbar />
 
       <div className="py-4">
-        <h2 className="heading-2 px-4">Added — 3</h2>
+        <h2 className="heading-2 px-4">
+          Added — {friends ? friends.friendsList.length : 0}
+        </h2>
         <ul>
-          {friends.map((friend) => (
-            <FriendItem
-              key={friend.userId}
-              friend={friend}
-              buttons={
-                <CircledIconButton
-                  icon={<IconChatBubble className="h-4 w-4" />}
-                />
-              }
-            />
+          {friends?.friendsList.map((friend) => (
+            <UserFriendItem key={friend.userId} friend={friend} />
           ))}
         </ul>
 
         <h2 className="heading-2 mt-4 px-4">Requests — 1</h2>
-        <ul>
-          <FriendItem
-            friend={createFriend()}
-            buttons={
-              <span className="flex gap-1.5">
-                <CircledIconButton
-                  icon={
-                    <IconCheck
-                      strokeWidth={2}
-                      className="h-4 w-4 text-jade-100"
-                    />
-                  }
-                />
-                <CircledIconButton
-                  icon={
-                    <IconXMark
-                      strokeWidth={2}
-                      className="h-4 w-4 text-crimson-100"
-                    />
-                  }
-                />
-              </span>
-            }
-          />
-        </ul>
+        <ul></ul>
       </div>
     </>
   );
