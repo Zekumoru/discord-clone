f5233e66cf1d0647da072eb9edeed03d02d0265b
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-03 21:33:28 +0200

    Implement profile picture

--- src/pages/channels/pages/friends/components/friend-item/FriendItem.tsx
+++ src/pages/channels/pages/friends/components/friend-item/FriendItem.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import useUser from '../../../../../../types/user/hooks/useUser';
 import removeTagFromName from '../../../../../../utils/removeTagFromName';
+import ProfilePicture from '../../../../components/ProfilePicture';
 
 type FriendItemProps = {
   friendId: string | undefined;
@@ -12,7 +13,7 @@ const FriendItem = ({ friendId, buttons }: FriendItemProps) => {
 
   return (
     <li className="li-rule-85 relative flex items-center gap-2 px-4 py-2">
-      <span className="inline-block h-10 w-10 rounded-full bg-neutral-600" />
+      <ProfilePicture user={friend} />
       <span className="mr-auto font-semibold">
         {removeTagFromName(friend?.username ?? '')}
       </span>
