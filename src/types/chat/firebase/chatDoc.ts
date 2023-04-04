5b336d481b86e94f399b7a3f2b3d17e4548daef3
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-04 16:34:44 +0200

    Fix wrong chat metadata db location

--- src/types/chat/firebase/chatDoc.ts
+++ src/types/chat/firebase/chatDoc.ts
@@ -2,7 +2,7 @@ import createDoc from '../../../utils/firebase/createDoc';
 import IChat from '../Chat';
 
 const chatDoc = (id: string) => {
-  return createDoc<IChat>(`chats/${id}`);
+  return createDoc<IChat>(`chats/${id}/metadata/metadata`);
 };
 
 export default chatDoc;
