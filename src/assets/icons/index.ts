6e1290330e342ec58ecbf024775eefc0c2c7a2f7
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-11 17:32:14 +0200

    Create remove friend partial modal

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -5,6 +5,14 @@ import IconDiscord from './IconDiscord';
 import IconUserPlus from './IconUserPlus';
 import IconXMark from './IconXMark';
 import IconUsers from './IconUsers';
+<<<<<<< HEAD
+=======
+import IconUserGroup from './IconUserGroup';
+import IconUserGroupOutline from './IconUserGroupOutline';
+import IconUserBars from './IconUserBars';
+import IconCog6Tooth from './IconCog6Tooth';
+import IconEllipsisHorizontal from './IconEllipsisHorizontal';
+>>>>>>> 7ce8bfa (Create remove friend partial modal)
 
 export {
   IconBurger,
@@ -14,4 +22,12 @@ export {
   IconUserPlus,
   IconXMark,
   IconUsers,
+<<<<<<< HEAD
+=======
+  IconUserGroup,
+  IconUserGroupOutline,
+  IconUserBars,
+  IconCog6Tooth,
+  IconEllipsisHorizontal,
+>>>>>>> 7ce8bfa (Create remove friend partial modal)
 };
---
+++ src/components/user-partial-modal/components/RemoveFriendPartialModal.tsx
@@ -0,0 +1,41 @@
+import { IconXMark } from '../../../assets/icons';
+import { PartialScreenModalMethods } from '../../../contexts/partial-screen-modal/PartialScreenModalContext';
+import ProfilePicture from '../../../pages/channels/components/ProfilePicture';
+import IUser from '../../../types/user/User';
+import extractNameAndTag from '../../../utils/extractNameAndTag';
+
+type RemoveFriendPartialModalProps = {
+  user: IUser | undefined;
+  close: PartialScreenModalMethods[1];
+};
+
+const RemoveFriendPartialModal = ({
+  user,
+  close,
+}: RemoveFriendPartialModalProps) => {
+  const [name] = extractNameAndTag(user?.username ?? '');
+
+  const handleRemoveFriend = () => {
+    console.log(`Removing ${name} from friends...`);
+  };
+
+  return (
+    <div className="w-full overflow-hidden rounded-t-lg bg-background-700">
+      <div className="flex items-center gap-2 p-4">
+        <ProfilePicture user={user} className="h-7 w-7 text-xs" />
+        <span className="font-semibold">{name}</span>
+        <span onClick={close} className="ml-auto text-silvergrey-300">
+          <IconXMark strokeWidth={2.2} className="h-6 w-6" />
+        </span>
+      </div>
+
+      <ul className="bg-background-500 font-semibold text-silvergrey-300">
+        <li onClick={handleRemoveFriend} className="p-4 text-salmon-100">
+          Remove Friend
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default RemoveFriendPartialModal;
