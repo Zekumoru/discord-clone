372fd7e46270eb2ba12bde97fa667dd7ffa80810
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-11 20:04:05 +0200

    Show current user on sidebar profile bar

--- src/contexts/sidebar/components/SidebarProfile.tsx
+++ src/contexts/sidebar/components/SidebarProfile.tsx
@@ -1,13 +1,21 @@
 import { IconCog6Tooth } from '../../../assets/icons';
+import ProfilePicture from '../../../pages/channels/components/ProfilePicture';
+import extractNameAndTag from '../../../utils/extractNameAndTag';
+import { useCurrentUser } from '../../current-user/CurrentUserContext';
 
 const SidebarProfile = () => {
+  const [user] = useCurrentUser();
+  const [name, tag] = extractNameAndTag(user?.username ?? '');
+
   return (
     <div className="col-span-2 flex items-center gap-3 border-t border-t-background-100 px-4 py-3 text-silvergrey-300">
-      <div className="h-10 w-10 shrink-0 rounded-full bg-background-100" />
+      <ProfilePicture user={user} className="h-10 w-10 shrink-0" />
 
       <div className="min-w-0">
-        <div className="truncate leading-none text-white">User</div>
-        <div className="text-xs">#0000</div>
+        <div className="truncate font-medium leading-none text-white">
+          {name}
+        </div>
+        <div className="text-xs">#{tag}</div>
       </div>
 
       <div className="ml-auto p-2">
