4541c0cbe8f773841caece068edd2b509108d628
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-13 00:47:12 +0200

    Auto-sign-in if already logged in before

--- src/pages/authentication/Authentication.tsx
+++ src/pages/authentication/Authentication.tsx
@@ -1,9 +1,20 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Logo from './components/Logo';
 import SignUp from './pages/SignUp';
+import { useCurrentUser } from '../../contexts/current-user/CurrentUserContext';
+import { useEffect } from 'react';
 
 const Authentication = () => {
+  const [currentUser] = useCurrentUser();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!currentUser) return;
+
+    navigate('/channels/@me');
+  }, [currentUser]);
+
   return (
     <div className="flex flex-col items-center px-4 py-5">
       <Logo className="mb-4 h-9" />
