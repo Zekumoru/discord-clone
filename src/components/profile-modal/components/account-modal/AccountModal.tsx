e719c302af23011a02a6135cfebe4d4d223c6798
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-14 03:50:04 +0200

    Design edit username modal

--- src/components/profile-modal/components/account-modal/AccountModal.tsx
+++ src/components/profile-modal/components/account-modal/AccountModal.tsx
@@ -1,10 +1,11 @@
-import { IconChevronLeft, IconChevronRight } from '../../../../assets/icons';
+import { IconChevronRight } from '../../../../assets/icons';
 import { useCurrentUser } from '../../../../contexts/current-user/CurrentUserContext';
 import { ScreenModalMethods } from '../../../../contexts/screen-modal/ScreenModalContext';
 import ScreenModalToolbar from '../../../../contexts/screen-modal/components/ScreenModalToolbar';
 import InsetList from '../../../modal-utils/InsetList';
 import InsetListItem from '../../../modal-utils/InsetListItem';
 import ModalChevronCloseButton from '../../../modal-utils/ModalChevronCloseButton';
+import EditUsernameListItem from './components/EditUsernameListItem';
 
 type AccountModalProps = {
   close: ScreenModalMethods[1];
@@ -27,13 +28,8 @@ const AccountModal = ({ close }: AccountModalProps) => {
 
       <div className="heading-2 mb-2 mt-8 px-4">Account Information</div>
       <InsetList>
-        <InsetListItem
-          className="ml-auto font-medium"
-          prefix={<span className="text-white">Username</span>}
-          postfix={<IconChevronRight className="h-4 w-4" strokeWidth={3} />}
-        >
-          {currentUser?.username}
-        </InsetListItem>
+        <EditUsernameListItem />
+
         <InsetListItem
           className="ml-auto font-medium normal-case"
           prefix={<span className="text-white">Email</span>}
