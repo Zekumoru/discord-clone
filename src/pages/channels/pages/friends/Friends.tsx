2ae8e6652885a1013b6b5e34590e64b759283395
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-15 23:54:02 +0200

    Add wumpus loading

--- src/pages/channels/pages/friends/Friends.tsx
+++ src/pages/channels/pages/friends/Friends.tsx
@@ -3,13 +3,16 @@ import FriendRequestsList from './components/FriendRequestsList';
 import FriendsList from './components/FriendsList';
 import FriendsToolbar from './components/FriendsToolbar';
 import NoFriendsDisplay from './components/NoFriendsDisplay';
+import WumpusLoadingDisplay from './components/WumpusLoadingDisplay';
 import useFriendRequests from './hooks/useFriendRequests';
 import useFriends from './hooks/useFriends';
 
 const Friends = () => {
-  const [user] = useCurrentUser();
-  const [friends] = useFriends(user?.friendsId);
-  const [friendRequests] = useFriendRequests(user?.friendRequestsId);
+  const [user, userLoading] = useCurrentUser();
+  const [friends, friendsLoading] = useFriends(user?.friendsId);
+  const [friendRequests, friendRequestsLoading] = useFriendRequests(
+    user?.friendRequestsId
+  );
 
   const hasFriends = !!friends?.friendsList.length;
   const hasRequests = !!friendRequests?.requests.length;
@@ -18,7 +21,9 @@ const Friends = () => {
     <>
       <FriendsToolbar />
 
-      {!hasFriends && !hasRequests ? (
+      {userLoading || friendsLoading || friendRequestsLoading ? (
+        <WumpusLoadingDisplay />
+      ) : !hasFriends && !hasRequests ? (
         <NoFriendsDisplay />
       ) : (
         <div className="py-4">
---
+++ src/pages/channels/pages/friends/components/WumpusLoadingDisplay.tsx
@@ -0,0 +1,14 @@
+const WumpusLoadingDisplay = () => {
+  return (
+    <div className="h-screen-toolbar flex flex-col items-center justify-center py-4">
+      <img
+        className="mb-4 h-40 w-40"
+        src="https://sticker-collection.com/stickers/animated/HelloWumpus/whatsapp/25a72969-c849-4d45-9b3d-3c88424cc00cfile_3674674.webp"
+        alt="Sad Wumpus"
+      />
+      <div className="font-medium text-silvergrey-300">Hold on tight!</div>
+    </div>
+  );
+};
+
+export default WumpusLoadingDisplay;
