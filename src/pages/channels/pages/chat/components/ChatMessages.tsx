c7fd33f4c5cd3b9707f21c1e0b294263232288ff
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-16 04:37:45 +0200

    Add messages date headers

--- src/pages/channels/pages/chat/components/ChatMessages.tsx
+++ src/pages/channels/pages/chat/components/ChatMessages.tsx
@@ -1,8 +1,10 @@
+import { format } from 'date-fns';
 import IUser from '../../../../../types/user/User';
 import extractNameAndTag from '../../../../../utils/extractNameAndTag';
 import ProfilePicture from '../../../components/ProfilePicture';
 import useMessages from '../hooks/useMessages';
 import ChatMessage from './ChatMessage';
+import { ReactNode } from 'react';
 
 type ChatMessagesProps = {
   user: IUser | undefined;
@@ -12,10 +14,11 @@ type ChatMessagesProps = {
 const ChatMessages = ({ user, chatId }: ChatMessagesProps) => {
   const [messages] = useMessages(chatId);
   const [name] = extractNameAndTag(user?.username ?? '');
+  let currentDateString = '';
 
   return (
     <div className="h-screen-toolbar flex flex-col justify-end p-4">
-      <div className="mb-2">
+      <div>
         <ProfilePicture user={user} className="mb-2 h-20 w-20" />
 
         <h2 className="text-2xl font-bold">{name}</h2>
@@ -26,14 +29,33 @@ const ChatMessages = ({ user, chatId }: ChatMessagesProps) => {
         </p>
       </div>
 
-      {messages && messages.length > 0 && (
-        <div className="mb-2.5 border-b-2 border-background-100" />
-      )}
-
       <ul className="mb-14">
-        {messages.map((message) => (
-          <ChatMessage key={message.id} message={message} />
-        ))}
+        {messages.map((message) => {
+          const dateString = message.timestamp
+            ? format(message.timestamp?.toDate(), 'PP')
+            : '';
+          let dateHeader: ReactNode = null;
+
+          if (dateString !== currentDateString) {
+            currentDateString = dateString;
+            dateHeader = (
+              <div className="my-2.5 flex items-center gap-2">
+                <div className="flex-1 border-b border-silvergrey-800" />
+                <div className="text-xs font-semibold text-silvergrey-400">
+                  {currentDateString}
+                </div>
+                <div className="flex-1 border-b border-silvergrey-800" />
+              </div>
+            );
+          }
+
+          return (
+            <>
+              {dateHeader}
+              <ChatMessage key={message.id} message={message} />
+            </>
+          );
+        })}
       </ul>
     </div>
   );
