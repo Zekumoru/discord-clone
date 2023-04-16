be80b81a3af978caf059cb28fdfcf96f6587c853
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-16 03:53:01 +0200

    Fix toolbar not sticky caused of overflow hidden

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import 'react-toastify/dist/ReactToastify.min.css';
 
 function App() {
   return (
-    <div className="App overflow-x-hidden">
+    <div className="App">
       <CurrentUserProvider>
         <ScreenModalProvider>
           <Routes>
