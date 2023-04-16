430dd9f6bef7f35162b1c5f6c21fa99815945d51
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-16 04:37:37 +0200

    Fix bottom messages see through

--- src/pages/channels/pages/chat/components/ChatInput.tsx
+++ src/pages/channels/pages/chat/components/ChatInput.tsx
@@ -38,23 +38,25 @@ const ChatInput = ({
         handleSend();
         e.preventDefault();
       }}
-      className={`fixed bottom-0 left-0 right-0 m-4 flex h-10 items-center gap-1 rounded bg-background-100 px-4 py-2 shadow-sm ${
+      className={`fixed -bottom-[1px] left-0 right-0 bg-background-300 p-4 pt-0 ${
         className ?? ''
       } ${isSidebarOpen ? '!-right-80 !left-80' : ''}`}
     >
-      <input
-        className="flex-1 bg-transparent outline-none placeholder:text-silvergrey-600"
-        value={value ?? ''}
-        onChange={(e) => onChange?.(e.target.value)}
-        onKeyUp={(e) => handleKeyDown(e.code)}
-        type="text"
-        placeholder={placeholder ?? ''}
-        disabled={disabled}
-      />
+      <div className="flex h-10 items-center gap-1 rounded bg-background-100 px-4 py-2 shadow-sm">
+        <input
+          className="flex-1 bg-transparent outline-none placeholder:text-silvergrey-600"
+          value={value ?? ''}
+          onChange={(e) => onChange?.(e.target.value)}
+          onKeyUp={(e) => handleKeyDown(e.code)}
+          type="text"
+          placeholder={placeholder ?? ''}
+          disabled={disabled}
+        />
 
-      <button>
-        <IconPaperAirplane className="h-6 w-6 text-warmblue-100" />
-      </button>
+        <button>
+          <IconPaperAirplane className="h-6 w-6 text-warmblue-100" />
+        </button>
+      </div>
     </form>
   );
 };
