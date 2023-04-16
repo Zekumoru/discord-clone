ab4088749c3b6ebc4b2118ecd8b096ac1bb4a088
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-16 03:22:07 +0200

    Go to login page if not signed in

--- src/pages/channels/Channels.tsx
+++ src/pages/channels/Channels.tsx
@@ -1,10 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import Friends from './pages/friends/Friends';
 import Chat from './pages/chat/Chat';
 import PartialScreenModalProvider from '../../contexts/partial-screen-modal/PartialScreenModalContext';
 import SidebarProvider from '../../contexts/sidebar/SidebarContext';
+import { useCurrentUser } from '../../contexts/current-user/CurrentUserContext';
+import { useEffect } from 'react';
 
 const Channels = () => {
+  const [user, loading] = useCurrentUser();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (loading) return;
+    if (user) return;
+
+    navigate('/');
+  }, [user, loading]);
+
   return (
     <PartialScreenModalProvider>
       <SidebarProvider>
