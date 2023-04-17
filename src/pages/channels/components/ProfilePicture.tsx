ef87515a76c03593bbad06b64d644e1525d67dbf
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-17 12:34:46 +0200

    Extract profile picture logic to a component

--- src/pages/channels/components/ProfilePicture.tsx
+++ src/pages/channels/components/ProfilePicture.tsx
@@ -1,6 +1,5 @@
+import RoundPicture from '../../../components/RoundPicture';
 import IUser from '../../../types/user/User';
-import uniqolor from 'uniqolor';
-import getStringColor from '../../../utils/getStringColor';
 
 type ProfilePictureProps = {
   user: IUser | undefined;
@@ -9,26 +8,13 @@ type ProfilePictureProps = {
 };
 
 const ProfilePicture = ({ user, className, onClick }: ProfilePictureProps) => {
-  const bgColor = getStringColor(user?.username);
-  const acronyms = [];
-
-  if (user?.username) {
-    const splits = user.username.split(' ').filter(Boolean);
-    acronyms.push(splits[0].substring(0, 1));
-    if (splits[1]) acronyms.push(splits[1].substring(0, 1));
-  }
-
   return (
-    <span
+    <RoundPicture
+      text={user?.username}
+      pictureUrl={user?.pictureUrl}
+      className={className}
       onClick={onClick}
-      className={`grid h-10 w-10 place-content-center rounded-full bg-cover bg-center bg-no-repeat font-medium ${className}`}
-      style={{
-        backgroundColor: bgColor,
-        backgroundImage: user?.pictureUrl ? `url(${user.pictureUrl})` : '',
-      }}
-    >
-      {!user?.pictureUrl && acronyms.join('')}
-    </span>
+    />
   );
 };
 
