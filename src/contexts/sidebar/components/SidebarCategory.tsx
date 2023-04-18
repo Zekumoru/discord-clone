b97c3fb4a47472255f20b1b811cc3bf1031babd9
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-18 03:20:04 +0200

    Implement collapsing channels in category

--- src/contexts/sidebar/components/SidebarCategory.tsx
+++ src/contexts/sidebar/components/SidebarCategory.tsx
@@ -1,4 +1,9 @@
-import { IconChevronDown, IconPlus } from '../../../assets/icons';
+import { useState } from 'react';
+import {
+  IconChevronDown,
+  IconChevronRight,
+  IconPlus,
+} from '../../../assets/icons';
 import ICategory from '../../../types/category/Category';
 import SidebarChannel from './SidebarChannel';
 
@@ -7,23 +12,42 @@ type SidebarCategoryProps = {
 };
 
 const SidebarCategory = ({ category }: SidebarCategoryProps) => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const collapseChannels = () => {
+    setCollapsed(!collapsed);
+  };
+
   return (
     <li className="mb-6 last-of-type:mb-2">
       <div className="mb-2 ml-1 mr-2 flex items-center gap-0.5 text-silvergrey-400">
-        <IconChevronDown className="h-3 w-3" strokeWidth={2} />
+        <button onClick={collapseChannels}>
+          {collapsed ? (
+            <IconChevronRight className="h-3 w-3" strokeWidth={2} />
+          ) : (
+            <IconChevronDown className="h-3 w-3" strokeWidth={2} />
+          )}
+        </button>
 
-        <div className="heading-2 text-silvergrey-400">{category.name}</div>
+        <div
+          onClick={collapseChannels}
+          className="heading-2 flex-1 text-silvergrey-400"
+        >
+          {category.name}
+        </div>
 
         <button className="ml-auto">
           <IconPlus className="h-4 w-4" strokeWidth={3} />
         </button>
       </div>
 
-      <ul>
-        {category.channels.map((channel) => (
-          <SidebarChannel key={channel.id} channel={channel} />
-        ))}
-      </ul>
+      {!collapsed && (
+        <ul>
+          {category.channels.map((channel) => (
+            <SidebarChannel key={channel.id} channel={channel} />
+          ))}
+        </ul>
+      )}
     </li>
   );
 };
