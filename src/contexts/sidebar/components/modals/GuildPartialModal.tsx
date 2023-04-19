d82896c74cc19b530afd8ba301389eb772c48153
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-19 23:48:44 +0200

    Open invite partial modal on click

--- src/contexts/sidebar/components/modals/GuildPartialModal.tsx
+++ src/contexts/sidebar/components/modals/GuildPartialModal.tsx
@@ -1,18 +1,29 @@
 import { IconCog6Tooth, IconUserPlus } from '../../../../assets/icons';
 import GuildPicture from '../../../../components/GuildPicture';
+import InvitePartialModal from '../../../../components/invite-partial-modal/InvitePartialModal';
 import useGuild from '../../../../types/guild/hooks/useGuild';
 import useMembers from '../../../../types/member/hooks/useMembers';
-import { PartialScreenModalProps } from '../../../partial-screen-modal/PartialScreenModalContext';
+import {
+  PartialScreenModalProps,
+  usePartialScreenModal,
+} from '../../../partial-screen-modal/PartialScreenModalContext';
 import PartialModalRoundedDiv from '../../../partial-screen-modal/components/PartialModalRoundedDiv';
 
 type GuildPartialModalProps = {
   guildId: string | undefined;
 } & PartialScreenModalProps;
 
-const GuildPartialModal = ({ guildId, close }: GuildPartialModalProps) => {
+const GuildPartialModal = ({ guildId }: GuildPartialModalProps) => {
+  const [openPartialModal, closePartialModal] = usePartialScreenModal();
   const [guild] = useGuild(guildId);
   const [members] = useMembers(guild?.membersId);
   const membersLength = members?.members.length ?? 0;
+
+  const openInvitePartialModal = () => {
+    openPartialModal(
+      <InvitePartialModal guild={guild} close={closePartialModal} />
+    );
+  };
 
   return (
     <div className="min-h-[85vh] w-full overflow-hidden rounded-t-lg bg-background-300">
@@ -33,7 +44,10 @@ const GuildPartialModal = ({ guildId, close }: GuildPartialModalProps) => {
         <div className="mt-2 border-b border-background-100" />
 
         <div className="grid grid-cols-2 px-4 py-2.5 text-silvergrey-300">
-          <button className="flex flex-col items-center gap-1.5 p-2 font-medium">
+          <button
+            onClick={openInvitePartialModal}
+            className="flex flex-col items-center gap-1.5 p-2 font-medium"
+          >
             <IconUserPlus className="h-7 w-7" />
             <div className="text-sm">Invite</div>
           </button>
