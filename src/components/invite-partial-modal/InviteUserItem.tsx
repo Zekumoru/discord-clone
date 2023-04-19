4d79c61e9864db381ee7a0a0f150f61d92b888cc
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-19 04:00:52 +0200

    Display "sent" instead of toast on invite sent

--- src/components/invite-partial-modal/InviteUserItem.tsx
+++ src/components/invite-partial-modal/InviteUserItem.tsx
@@ -6,6 +6,7 @@ import extractNameAndTag from '../../utils/extractNameAndTag';
 import IInvite from '../../types/invite/Invite';
 import { useCurrentUser } from '../../contexts/current-user/CurrentUserContext';
 import LoadingScreen from '../LoadingScreen';
+import { useState } from 'react';
 
 type InviteUserItemProps = {
   user: IUser;
@@ -15,8 +16,9 @@ type InviteUserItemProps = {
 const InviteUserItem = ({ user, invite }: InviteUserItemProps) => {
   const [currentUser] = useCurrentUser();
   const [name, tag] = extractNameAndTag(user.username);
+  const [sent, setSent] = useState(false);
   const { mutate: sendInvite, isLoading } = useSendInvite({
-    onSuccess: () => toast.success('Invitation sent!'),
+    onSuccess: () => setSent(true),
     onError: () => toast.error('Could not send invite!'),
   });
 
@@ -47,12 +49,16 @@ const InviteUserItem = ({ user, invite }: InviteUserItemProps) => {
           <span>#{tag}</span>
         </div>
 
-        <button
-          onClick={handleSendInvite}
-          className="ml-auto rounded-sm bg-background-100 px-4 py-1.5 text-sm font-semibold text-white"
-        >
-          Invite
-        </button>
+        {sent ? (
+          <div className="ml-auto text-silvergrey-400">Sent</div>
+        ) : (
+          <button
+            onClick={handleSendInvite}
+            className="ml-auto rounded-sm bg-background-100 px-4 py-1.5 text-sm font-semibold text-white"
+          >
+            Invite
+          </button>
+        )}
       </div>
     </li>
   );
