68db2fc9b04a4060474f445d9be3468d6d073d97
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-21 18:17:10 +0200

    Fix sidebar profile not opening user preview modal

--- src/contexts/sidebar/components/SidebarProfile.tsx
+++ src/contexts/sidebar/components/SidebarProfile.tsx
@@ -1,14 +1,29 @@
+import { toast } from 'react-toastify';
 import { IconCog6Tooth } from '../../../assets/icons';
 import ProfileModal from '../../../components/profile-modal/ProfileModal';
+import UserPartialModal from '../../../components/user-partial-modal/UserPartialModal';
 import ProfilePicture from '../../../pages/channels/components/ProfilePicture';
 import extractNameAndTag from '../../../utils/extractNameAndTag';
 import { useCurrentUser } from '../../current-user/CurrentUserContext';
+import { usePartialScreenModal } from '../../partial-screen-modal/PartialScreenModalContext';
 import { useScreenModal } from '../../screen-modal/ScreenModalContext';
 
 const SidebarProfile = () => {
   const [user] = useCurrentUser();
   const [name, tag] = extractNameAndTag(user?.username ?? '');
   const [openModal, closeModal] = useScreenModal();
+  const [openPartialModal, closePartialModal] = usePartialScreenModal();
+
+  const handleOpenUserPartialModal = () => {
+    if (!user) {
+      toast.error('Could not open user modal!');
+      return;
+    }
+
+    openPartialModal(
+      <UserPartialModal userId={user.id} close={closePartialModal} />
+    );
+  };
 
   const handleOpenProfileModal = () => {
     openModal(<ProfileModal close={closeModal} />);
@@ -17,11 +32,12 @@ const SidebarProfile = () => {
   return (
     <div className="col-span-2 flex items-center gap-3 border-t border-t-background-100 px-4 py-3 text-silvergrey-300">
       <ProfilePicture
+        onClick={handleOpenUserPartialModal}
         user={user}
         className="h-10 w-10 shrink-0 text-lg text-white"
       />
 
-      <div className="min-w-0">
+      <div onClick={handleOpenUserPartialModal} className="min-w-0">
         <div className="truncate font-medium leading-none text-white">
           {name}
         </div>
