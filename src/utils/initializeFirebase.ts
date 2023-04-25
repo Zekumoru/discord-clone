064c1ab4cc59aa4d103a56d8706777e90c97552f
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-26 01:11:35 +0200

    Use emulators

--- src/utils/initializeFirebase.ts
+++ src/utils/initializeFirebase.ts
@@ -1,4 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import firebaseConfig from '../firebase.config';
+import { connectAuthEmulator, getAuth } from 'firebase/auth';
+import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
+import { connectStorageEmulator, getStorage } from 'firebase/storage';
 
 initializeApp(firebaseConfig);
+
+if (import.meta.env.VITE_EMULATORS_ENABLED === 'true') {
+  connectAuthEmulator(getAuth(), 'http://localhost:3173/', {
+    disableWarnings: true,
+  });
+  connectFirestoreEmulator(getFirestore(), 'localhost', 3174);
+  connectStorageEmulator(getStorage(), 'localhost', 9199);
+  console.warn('App is currently using emulators.');
+}
