7fabb3f82f1d96e62e916402c0ee2d4a2ebe0ca5
Zekumoru Dragonhart <dev@zekumoru.com>
2023-04-27 02:27:44 +0200

    Fix sidebar showing loading after account create

--- src/types/user-chat/hooks/useUserChats.tsx
+++ src/types/user-chat/hooks/useUserChats.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from 'react-query';
 import userChatsDoc from '../firebase/userChatsDoc';
 import { getDoc } from 'firebase/firestore';
+import { useEffect, useRef } from 'react';
+import { queryClient } from '../../../components/QueryClientInitializer';
 
 const getUserChats = async (userChatsId: string | undefined) => {
   if (userChatsId === undefined) return;
@@ -10,17 +12,29 @@ const getUserChats = async (userChatsId: string | undefined) => {
 };
 
 const useUserChats = (userChatsId: string | undefined) => {
+  const triesRef = useRef(-1);
   const {
     data: userChat,
     isLoading,
     error,
   } = useQuery(
     ['user-chats', userChatsId],
-    async () => await getUserChats(userChatsId),
+    async () => {
+      triesRef.current += 0;
+      return await getUserChats(userChatsId);
+    },
     {
       enabled: !!userChatsId,
     }
   );
+
+  useEffect(() => {
+    if (userChat !== undefined) return;
+
+    if (triesRef.current >= 0 && triesRef.current <= 3) {
+      queryClient.invalidateQueries(['user-chats', userChatsId]);
+    }
+  }, [userChat]);
 
   return [userChat, isLoading, error] as const;
 };
--- src/types/user/hooks/useUserGuilds.tsx
+++ src/types/user/hooks/useUserGuilds.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from 'react-query';
 import userGuildsDoc from '../firebase/userGuildsDoc';
 import { getDoc } from 'firebase/firestore';
+import { useEffect, useRef } from 'react';
+import { queryClient } from '../../../components/QueryClientInitializer';
 
 const getUserGuilds = async (userGuildsId: string) => {
   const userGuildsRef = userGuildsDoc(userGuildsId);
@@ -8,17 +10,29 @@ const getUserGuilds = async (userGuildsId: string) => {
 };
 
 const useUserGuilds = (userGuildsId: string | undefined) => {
+  const triesRef = useRef(-1);
   const {
     data: userGuilds,
     isLoading,
     error,
   } = useQuery(
     ['user-guilds', userGuildsId],
-    async () => await getUserGuilds(userGuildsId!),
+    async () => {
+      triesRef.current += 0;
+      return await getUserGuilds(userGuildsId!);
+    },
     {
       enabled: !!userGuildsId,
     }
   );
+
+  useEffect(() => {
+    if (userGuilds !== undefined) return;
+
+    if (triesRef.current >= 0 && triesRef.current <= 3) {
+      queryClient.invalidateQueries(['user-guilds', userGuildsId]);
+    }
+  }, [userGuilds]);
 
   return [userGuilds, isLoading, error] as const;
 };
